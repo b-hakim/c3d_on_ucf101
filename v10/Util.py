5a4ec66f490d27ca6383c769fe6430b9fd6d8ddc
Bassel Safwat <b.safwat@starkbits.com>
2017-11-11 00:56:17 +0200

    Add functionalities in Utilities

--- v10/Util.py
+++ v10/Util.py
@@ -1,5 +1,8 @@
 import os
 
+import pathlib
+
+
 def get_file_name_from_path_without_extention(file_path):
     return file_path[file_path.rfind('/') + 1:file_path.rfind('.')]
 
@@ -21,6 +24,16 @@ def get_direct_folder_containing_file(file_path):
 def is_same_group(file1_path, file2_path):
     return file1_path[0:-2] == file2_path[0:-2]
 
+
+def get_all_files_with_extenstion(path, ext):
+    return list(map(lambda x: str(x), list(pathlib.Path(path).glob("*"+ext))))
+
+
+def get_immediate_subdirectories(a_dir, full_path):
+    if not full_path:
+        return [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
+    else:
+        return [os.path.join(a_dir, name) for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
 
 # test get_file_name_from_path_without_extension
 # print 'file without extension: ', \
