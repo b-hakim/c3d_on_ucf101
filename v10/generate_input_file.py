362c52f3ab53e8151f1a777c963f774206c08a85
Bassel Safwat <b.safwat@starkbits.com>
2017-11-11 01:39:57 +0200

    debug msg

--- v10/generate_input_file.py
+++ v10/generate_input_file.py
@@ -40,7 +40,7 @@ def ParseArgs():
 
 if __name__ == '__main__':
     args = ParseArgs()
-    print args.isTest == True
+    print bool(args.isTest) == True
     generate_input_file(args.ucf101_input_file_path, args.base_dir_frm, args.output_file_path, bool(args.isTest))
 
 #python v10/generate_input_file.py "/root/repos/c3d_on_ucf101/ucfTrainTestlist/trainlist01.txt" "/root/sources/C3D/C3D-v1.0/data/ucf101/frm/" "/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/prototxt/input_list_frm_train01.txt", False
