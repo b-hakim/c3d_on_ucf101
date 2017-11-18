62681f29b7d477d44da012261bf36cf0c9cc4093
Bassel Safwat <b.safwat@starkbits.com>
2017-11-19 01:54:03 +0200

    use the right datastructure using array instead of directly using numpy

--- scikit-learn-mlp/display_feature.py
+++ scikit-learn-mlp/display_feature.py
@@ -1,11 +1,12 @@
 import numpy as np
+import array
 
 def display_feature(feature_file):
     with  open(feature_file, "rb") as f:
         line = f.read()
 
-    (n, c, l, h, w) = np.array("i", line[:20])
-    feature_vec = np.array("f", line[20:])
+    (n, c, l, h, w) = array.array("i", line[:20])
+    feature_vec = np.array(array.array("f", line[20:]))
 
     print  (n, c, l, h, w)
 
