b2586a6365bf5c6b1fa6cefe4c9891cbfaa399fe
Bassel Safwat <b.safwat@starkbits.com>
2017-11-19 03:13:00 +0200

    adjust paths for server returns after loading all training data on memory

--- scikit-learn-mlp/mlp_prepare_data.py
+++ scikit-learn-mlp/mlp_prepare_data.py
@@ -47,7 +47,7 @@ def get_training_matrix(output_features, features_directory, class_index, featur
     print 'passs:', passes
     return np.array(ret_x), np.array(ret_y)
 
-def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace):
+def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace, server_path=False):
     feature_layer, lst_feature_layer = utl.adjust_feature_name(_feature_layer)
 
     print 'experiment settings:', str(num_hidden_layers), 'hidden units, classify on local features with settings:'+feature_layer+'_'+solver
@@ -59,12 +59,24 @@ def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace):
     y_test = None
 
     if force_replace or not os.path.isfile(filename) or not os.path.isfile("data_test_sampled/y_"+feature_layer+".txt"):
-        output_features = '/home/kasparov092/sources/c3d/v1.0/examples/c3d_feature_extraction/c3d_sports_finetuned_ucf_model/output_list_prefix.txt'
+        if not server_path:
+            output_features = '/home/kasparov092/sources/c3d/v1.0/examples/c3d_feature_extraction/c3d_sports_finetuned_ucf_model/output_list_prefix.txt'
+        else:
+            output_features = '/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/prototxt/output_list_prefix_train01.txt'
+
         #output_features = '/home/kasparov092/sources/c3d/v1.0/examples/c3d_feature_extraction/c3d_sports_finetuned_ucf_model/output_train_list_prefix.txt'
         #features_directory = '/media/kasparov092/64C6CF47C6CF1866/ubuntu-c3d-examples-outputtraining/'
-        features_directory = '/home/kasparov092/sources/c3d/v1.0/examples/c3d_feature_extraction/'
+        if not server_path:
+            features_directory = '/home/kasparov092/sources/c3d/v1.0/examples/c3d_feature_extraction/'
+        else:
+            features_directory = '/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/'
+
         #output/c3d/v_ApplyEyeMakeup_g01_c05/000001
-        class_index_path = '/home/kasparov092/sources/c3d/v1.0/data/ucf101/ucfTrainTestlist/classInd.txt'
+        if not server_path:
+            class_index_path = '/home/kasparov092/sources/c3d/v1.0/data/ucf101/ucfTrainTestlist/classInd.txt'
+        else:
+            class_index_path = '/root//home/kasparov092/sources/c3d/v1.0/data/ucf101/ucfTrainTestlist/classInd.txt'
+
         class_index = {}
 
         with open(class_index_path) as f:
@@ -73,17 +85,20 @@ def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace):
                class_index[ic[1]] = int(ic[0])
 
         x, y = get_training_matrix(output_features, features_directory, class_index, lst_feature_layer)
+        print 'all training data fit into memory, press a key to continue'
+        input()
+
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42)
 
         print ('saving data to disk... ')
 
-        np.savetxt("data_test_sampled/x_"+feature_layer+".txt", x) # the save is trained on sample of testing
-        np.savetxt("data_test_sampled/y_"+feature_layer+".txt", y)
+        np.savetxt("data_train_sampled/x_"+feature_layer+".txt", x) # the save is trained on sample of testing
+        np.savetxt("data_train_sampled/y_"+feature_layer+".txt", y)
 
-        np.savetxt("data_test_sampled/x_local_train_"+feature_layer+".txt", x_train) # the save is trained on sample of testing
-        np.savetxt("data_test_sampled/y_local_train_"+feature_layer+".txt", y_train)
-        np.savetxt("data_test_sampled/x_local_test_"+feature_layer+".txt", x_test) # the save is trained on sample of testing
-        np.savetxt("data_test_sampled/y_local_test_"+feature_layer+".txt", y_test)
+        np.savetxt("data_train_sampled/x_local_train_"+feature_layer+".txt", x_train) # the save is trained on sample of testing
+        np.savetxt("data_train_sampled/y_local_train_"+feature_layer+".txt", y_train)
+        np.savetxt("data_train_sampled/x_local_test_"+feature_layer+".txt", x_test) # the save is trained on sample of testing
+        np.savetxt("data_train_sampled/y_local_test_"+feature_layer+".txt", y_test)
 
         print 'initializing the mlp classifier...'
 
@@ -104,9 +119,9 @@ def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace):
         pickle.dump(clf, open(filename, 'wb'))
     else:
         # load the model from disk
-        x_train = np.loadtxt("data_test_sampled/x_local_train_" + feature_layer + ".txt")
-        x_test = np.loadtxt("data_test_sampled/x_local_test_" + feature_layer + ".txt")
-        y_test = np.loadtxt("data_test_sampled/y_local_test_" + feature_layer + ".txt")
+        x_train = np.loadtxt("data_train_sampled/x_local_train_" + feature_layer + ".txt")
+        x_test = np.loadtxt("data_train_sampled/x_local_test_" + feature_layer + ".txt")
+        y_test = np.loadtxt("data_train_sampled/y_local_test_" + feature_layer + ".txt")
 
         print 'preprocessing: scaling the test data ...'
         mean_local = x_train.mean(axis=0)
@@ -123,7 +138,7 @@ def make_experiments(num_hidden_layers, _feature_layer, solver, force_replace):
     np.savetxt("data_test/testing_predictions_"+feature_layer+".txt", testing_predictions)
     utl.calculate_accuracy(testing_predictions, y_test)
 
-make_experiments(5000, ['fc6','fc8'], 'lbfgs', True)
+make_experiments(50, ['pool5'], 'adam', True, True)
 exit()
 make_experiments(50, ['fc8'], 'adam', True)
 make_experiments(50, ['fc6'], 'lbfgs', True)
