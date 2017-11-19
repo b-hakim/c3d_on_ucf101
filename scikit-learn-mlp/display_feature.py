31bd63b501001b8be808e6b8a108c92d7d7aadfc
Bassel Safwat <b.safwat@starkbits.com>
2017-11-19 02:06:11 +0200

    display first pool values

--- scikit-learn-mlp/display_feature.py
+++ scikit-learn-mlp/display_feature.py
@@ -9,10 +9,12 @@ def display_feature(feature_file):
     feature_vec = np.array(array.array("f", line[20:]))
 
     print  (n, c, l, h, w)
+    return feature_vec
 
 if __name__ == '__main__':
     print 'pool5'
-    display_feature('/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/output/c3d/v_YoYo_g25_c05/000001.pool5')
+    pool5 = display_feature('/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/output/c3d/v_YoYo_g25_c05/000001.pool5')
+    print pool5[0,0,0,...]
     print 'fc6'
     display_feature('/root/sources/C3D/C3D-v1.0/examples/c3d_feature_extraction/output/c3d/v_YoYo_g25_c05/000001.fc6')
     print 'fc8'
